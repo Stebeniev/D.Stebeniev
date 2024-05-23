70be0b921f0a9f3af80b2d8e59ef68bc6da8ffff
Dmitriy <dm.stebenev@gmail.com>
2024-05-23 22:33:16 +0300

    added files

---
+++ main.js
@@ -0,0 +1,51 @@
+document.addEventListener('DOMContentLoaded', function() {
+  function addShowClassWithDelay(elements, delay) {
+    elements.forEach((element, index) => {
+      setTimeout(() => {
+        element.classList.add('show');
+      }, index * delay);
+    });
+  }
+
+  const mainElements = [
+    document.querySelector('.hello'),
+    document.querySelector('.everyone'),
+    document.querySelector('.title-content__main'),
+    document.querySelector('.title-content__text')
+  ];
+
+  const skillsItems = document.querySelectorAll('.skills-item');
+  const educationItems = document.querySelectorAll('.education-item');
+  const contactsItems = document.querySelectorAll('.contacts-item');
+
+  addShowClassWithDelay(mainElements, 1000);
+  addShowClassWithDelay(Array.from(skillsItems), 1000);
+  addShowClassWithDelay(Array.from(educationItems), 1000);
+  addShowClassWithDelay(Array.from(contactsItems), 1000);
+});
+
+// document.addEventListener('DOMContentLoaded', function() {
+//   const burgerMenu = document.querySelector('.burger-menu');
+//   const navList = document.querySelector('.nav-list');
+//
+//   burgerMenu.addEventListener('click', function() {
+//     navList.classList.toggle('nav-list--open');
+//   });
+// });
+document.addEventListener('DOMContentLoaded', function() {
+  const burgerMenu = document.querySelector('.burger-menu');
+  const navList = document.querySelector('.nav-list');
+
+  burgerMenu.addEventListener('click', function() {
+    burgerMenu.classList.toggle('is-active');
+    navList.classList.toggle('nav-list--open');
+  });
+
+  const navLinks = document.querySelectorAll('.nav-list__link');
+  navLinks.forEach(link => {
+    link.addEventListener('click', function() {
+      burgerMenu.classList.remove('is-active');
+      navList.classList.remove('nav-list--open');
+    });
+  });
+});
