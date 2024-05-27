a509cd98318a75b8b7bc114ca5076365f050300c
Dmitriy <dm.stebenev@gmail.com>
2024-05-27 12:17:50 +0300

    fix: index.html

--- main.js
+++ main.js
@@ -30,16 +30,33 @@ document.addEventListener('DOMContentLoaded', function() {
   const burgerMenu = document.querySelector('.burger-menu');
   const navList = document.querySelector('.nav-list');
 
-  burgerMenu.addEventListener('click', function() {
-    burgerMenu.classList.toggle('is-active');
-    navList.classList.toggle('nav-list--open');
-  });
-
-  const navLinks = document.querySelectorAll('.nav-list__link');
-  navLinks.forEach(link => {
-    link.addEventListener('click', function() {
-      burgerMenu.classList.remove('is-active');
-      navList.classList.remove('nav-list--open');
+  if (burgerMenu && navList) {
+    burgerMenu.addEventListener('click', function() {
+      burgerMenu.classList.toggle('is-active');
+      navList.classList.toggle('nav-list--open');
     });
-  });
+
+    const navLinks = document.querySelectorAll('.nav-list__link');
+    navLinks.forEach(link => {
+      link.addEventListener('click', function() {
+        burgerMenu.classList.remove('is-active');
+        navList.classList.remove('nav-list--open');
+      });
+    });
+  }
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+  const downloadBtn = document.getElementById('download-btn');
+
+  if (downloadBtn) {
+    downloadBtn.addEventListener('click', function() {
+      const link = document.createElement('a');
+      link.href = './CV_Dmitriy-Stebeniev.pdf';
+      link.download = 'resume Dmitriy_Stebeniev.pdf';
+      document.body.appendChild(link);
+      link.click();
+      document.body.removeChild(link);
+    });
+  }
 });
